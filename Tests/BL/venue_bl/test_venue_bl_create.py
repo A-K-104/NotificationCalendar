689c250546a039078ee38acc8ef92c362518eaf6
Asaf.Kostiner <Asaf.Kostiner@methoda.com>
2023-12-16 14:42:45 +0200

    Create venue bl uni tests

---
+++ Tests/BL/venue_bl/test_venue_bl_create.py
@@ -0,0 +1,36 @@
+from unittest.mock import patch
+import unittest
+
+from flask import Flask
+
+from BL import venue_bl
+from Commons.Exceptions.MissingValueException import MissingValueException
+from Commons.Exceptions.NameAlreadyUsedException import NameAlreadyUsedException
+
+
+class TestCreateVenue(unittest.TestCase):
+
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.valid_json = {'room_name': '404 TLV'}
+
+    @patch('Models.venue_model.create_venue')
+    def test_create_venue_success(self, mock_create_venue):
+        with self.app.app_context():
+            mock_create_venue.return_value = self.valid_json
+            response = venue_bl.create_venue_bl(self.valid_json)
+            self.assertEqual(response.json, self.valid_json)
+
+    def test_create_venue_missing_room_name(self):
+        with self.assertRaises(MissingValueException):
+            venue_bl.create_venue_bl({})
+
+    @patch('Models.venue_model.create_venue')
+    def test_create_venue_duplicate_key_exception(self, mock_create_venue):
+        mock_create_venue.side_effect = Exception("duplicate key value")
+        with self.assertRaises(NameAlreadyUsedException):
+            venue_bl.create_venue_bl(self.valid_json)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Tests/BL/venue_bl/test_venue_bl_update.py
@@ -0,0 +1,38 @@
+from unittest.mock import patch
+import unittest
+
+from flask import Flask
+
+from BL import venue_bl
+from Commons.Exceptions.NameAlreadyUsedException import NameAlreadyUsedException
+from Commons.Exceptions.NotFoundException import NotFoundException
+
+
+class TestUpdateVenue(unittest.TestCase):
+
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.valid_json = {'room_name': '404 TLV'}
+
+    @patch('Models.venue_model.update_venue_by_id')
+    def test_update_venue_success(self, mock_update_venue):
+        with self.app.app_context():
+            mock_update_venue.return_value = self.valid_json
+            response = venue_bl.update_venue_bl(1, self.valid_json)
+            self.assertEqual(response.json, self.valid_json)
+
+    @patch('Models.venue_model.update_venue_by_id')
+    def test_update_venue_missing_room_name(self, mock_update_venue):
+        mock_update_venue.return_value = None
+        with self.assertRaises(NotFoundException):
+            venue_bl.update_venue_bl(1, self.valid_json)
+
+    @patch('Models.venue_model.update_venue_by_id')
+    def test_update_venue_duplicate_key_exception(self, mock_update_venue):
+        mock_update_venue.side_effect = Exception("duplicate key value")
+        with self.assertRaises(NameAlreadyUsedException):
+            venue_bl.update_venue_bl(1, self.valid_json)
+
+
+if __name__ == '__main__':
+    unittest.main()
