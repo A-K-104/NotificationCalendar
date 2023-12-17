ef1bf7e806dbdf8d2dc2354bc045e2d2d8ea2acc
Asaf.Kostiner <Asaf.Kostiner@methoda.com>
2023-12-17 23:19:00 +0200

    Fixed unitest for the new function

--- Test/BL/event_bl/TestCreateEvent.py
+++ Test/BL/event_bl/TestCreateEvent.py
@@ -1,5 +1,6 @@
 import datetime
 import unittest
+from unittest.mock import patch
 
 from flask import Flask
 
@@ -14,10 +15,15 @@ class TestCreateEvent(TestCreateOneBase):
     def setUp(self):
         self.app = Flask(__name__)
         self.element_bl = EventBL()
-        self.valid_json = {'title': 'The 404 party', 'date': '20 Feb 1991 00:00:00 GMT'}
+        self.valid_json = {'title': 'The 404 party', 'date': '20 Feb 1991 00:00:00 GMT', 'notifications': 1800,
+                           'organizer': 1}
         self.model_name = 'EventModel.EventModel'
         user: UserDTO = UserDTO(1, "WebWizard", "ADMIN", datetime.datetime.now())
         self.additional_data = [user]
+
+    def test_create_one_should_success(self):
+        with patch(f'BL.EventBL.EventBL.post_event_creation', return_value=None):
+            super().test_create_one_should_success()
 
 
 if __name__ == '__main__':
